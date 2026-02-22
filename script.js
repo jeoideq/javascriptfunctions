897a97213807b9371145ccded1083eae9d789dad
jeoideq <hanssonj20@gmail.com>
2026-02-22 12:30:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function get_values()
 {
     let username=document.getElementById("username").value;
-
+    let year=document.getElementById("year").value;
+    let adress=document.getElementById("adress").value;
+    let pincode=document.getElementById("pincode").value;
+    let message=document.getElementById("message");
+    message.innerHTML="you are logged in";
 
 }
+
